0fa6ad4258394487ca9bbd6fb047e021f1889d87
Benson Njuguna K <bennspencer@gmail.com>
2020-07-26 17:23:53 +0300

    Core app component design

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Typography } from '@material-ui/core';
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
+import { fetchData } from './api';
+import coronaImage from './images/covid-logo.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        data: {},
+    }
+
+    async componentDidMount() {
+        const fetchedData = await fetchData();
+        this.setState({ data: fetchedData })
+        // console.log(data);
+    }
+
+    handleCountrySelection = async (country) => {
+        const fetchedData = await fetchData(country);
+        console.log(fetchedData);
+        this.setState({ data: fetchedData, country: country })
+    }
+
+
+    render() {
+        const { data, country } = this.state;
+        return (
+            <div className={styles.container}>
+                <img className={styles.image} src={coronaImage} alt='covid-19' />
+                <Typography>
+                    <h1>Daily Updates</h1>
+                </Typography>
+                <Cards data={data} />
+                <CountryPicker handleCountrySelection={this.handleCountrySelection} />
+                <Chart data={data} country={country} />
+            </div>
+        );
+    }
+
 }
-
 export default App;
